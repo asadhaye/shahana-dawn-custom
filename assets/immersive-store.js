bb2b6f5ab95f354c626a8edb7392fe0732992e7a
asadhaye <asadhaye@gmail.com>
2026-05-05 13:19:10 +0500

    Fix syntax error in noise function - extra closing parenthesis

--- assets/immersive-store.js
+++ assets/immersive-store.js
@@ -806,7 +806,7 @@ const fragmentShaderSource = `
   uniform float uTime;
 
   float noise(vec2 co) {
-    return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
+    return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453;
   }
 
   vec2 parallaxUv(vec2 uv, sampler2D depthTex, vec2 mouse) {
